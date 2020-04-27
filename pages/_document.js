3e7bb4eb9690af5e2c81abf3f92af1b86930e31c
romanfilenko <filenko@uxf.cz>
2020-04-27 13:49:41 +0200

    separate favicon file

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Main, NextScript, Head } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import { LayoutFavicon } from '../components/layouts/LayoutFavicon';
 
 export default class MyDocument extends Document {
   render() {
@@ -8,13 +9,12 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="description" content="build with nextjs" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="theme-color" content="#38cfd9"></meta>
-          <link rel="shortcut icon" type="image/ico" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700&family=Merriweather:wght@400;700&display=swap"
             rel="stylesheet"
           />
         </Head>
+        <LayoutFavicon />
         <body>
           <Main />
           <NextScript />
