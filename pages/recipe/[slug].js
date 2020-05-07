b3664c8d6e52e4a479f2e417cc6880ca320ee5e7
romanfilenko <filenko@uxf.cz>
2020-05-07 13:56:00 +0200

    refactor recipe page

--- pages/recipe/[slug].js
+++ pages/recipe/[slug].js
@@ -1,14 +1,9 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
-import fetch from 'isomorphic-fetch';
+import { RECIPES_LIST } from '../../data/recipes';
 import { Image, Transformation } from 'cloudinary-react';
-import { RecipesContext } from 'components/contexts/RecipesContext';
-import BaseLayout from 'components/layouts/BaseLayout';
-import { useRouter } from 'next/router';
-import { IoMdTimer } from 'react-icons/io';
-import { FaShareAlt } from 'react-icons/fa';
-import { GiKnifeFork, GiFruitBowl, GiBubblingBowl } from 'react-icons/gi';
 import { theme } from 'components/styled/theme';
+import BaseLayout from 'components/layouts/BaseLayout';
 import {
   StyledContainer,
   StyledButton,
@@ -20,12 +15,12 @@ import {
   StyledFlex,
 } from 'components/styled';
 
-const Recipe = (props) => {
-  const { recipesList } = useContext(RecipesContext);
-  const [recipe, setRecipe] = useState(null);
-  const router = useRouter();
-  const { slug } = router.query;
-  let recipeItem;
+import { IoMdTimer } from 'react-icons/io';
+import { FaShareAlt } from 'react-icons/fa';
+import { GiKnifeFork, GiFruitBowl, GiBubblingBowl } from 'react-icons/gi';
+
+const Recipe = ({ currentRecipe }) => {
+  const [recipe, setRecipe] = useState(currentRecipe);
 
   const shareAPI = () => {
     if (navigator.share) {
@@ -40,148 +35,128 @@ const Recipe = (props) => {
         .catch((error) => console.log('Error sharing', error));
     }
   };
-  useEffect(() => {
-    recipeItem = slug - 1;
-    let currRecipe = recipesList[recipeItem];
-    setRecipe(currRecipe);
-  }, []);
-
   if (!recipe) return <p></p>;
 
   return (
-    <>
-      <BaseLayout title={`${recipe.slugUrl} details`}>
-        <StyledContainer>
-          <StyledRecipeDetail>
-            <StyledH2>{recipe.name}</StyledH2>
-            <p dangerouslySetInnerHTML={{ __html: recipe.description }} />
-            <div className="prep_time">
-              <p>
-                <IoMdTimer size="1.25rem" color={theme.colors.text} />
-                preparation - {recipe.time_prep},
-              </p>
-              <p> time to cook - {recipe.time_cook}, </p>
-              <p> total - {recipe.time_total}</p>
-            </div>
+    <BaseLayout title={`${recipe.slugUrl} details`}>
+      <StyledContainer>
+        <StyledRecipeDetail>
+          <StyledH2>{recipe.name}</StyledH2>
+          <p dangerouslySetInnerHTML={{ __html: recipe.description }} />
+          <div className="prep_time">
+            <p>
+              <IoMdTimer size="1.25rem" color={theme.colors.text} />
+              preparation - {recipe.time_prep},
+            </p>
+            <p> time to cook - {recipe.time_cook}, </p>
+            <p> total - {recipe.time_total}</p>
+          </div>
 
-            <div className="tags">
-              <p>
-                tags:
-                {Object.values(recipe.tags).map((value, index) => {
-                  return (
-                    <StyledTag key={index} as="span">
-                      {value}
-                    </StyledTag>
-                  );
-                })}
-              </p>
+          <div className="tags">
+            <p>
+              tags:
+              {Object.values(recipe.tags).map((value, index) => {
+                return (
+                  <StyledTag key={index} as="span">
+                    {value}
+                  </StyledTag>
+                );
+              })}
+            </p>
+          </div>
+          <div className="desc">
+            <div className="recipe_img">
+              {!recipe.image && <p>no image </p>}
+              <Image
+                cloudName={process.env.CLOUDINARY_CLOUD_NAME}
+                publicId={recipe.slugUrl}
+                alt={recipe.name.toLowerCase()}
+              >
+                <Transformation fetchFormat="auto" />
+              </Image>
             </div>
-            <div className="desc">
-              <div className="recipe_img">
-                {!recipe.image && <p>no image </p>}
-                <Image
-                  cloudName={process.env.CLOUDINARY_CLOUD_NAME}
-                  publicId={recipe.slugUrl}
-                  alt={recipe.name.toLowerCase()}
-                >
-                  <Transformation fetchFormat="auto" />
-                </Image>
-              </div>
-              <div>
-                <StyledText>
-                  <GiFruitBowl size="1.25rem" color={theme.colors.text} />{' '}
-                  Ingridients:
-                </StyledText>
-                {recipe.servings && (
-                  <span className="serves">
-                    (for {recipe.servings} servings{' '}
-                    <GiKnifeFork size=".75rem" color={theme.colors.mutedText} />
-                    )
-                  </span>
-                )}
-                <ul>
-                  {Object.values(recipe.ingridients).map((value, index) => {
-                    return <li key={index}>{value}</li>;
-                  })}
-                </ul>
-
-                {recipe.modifications && (
-                  <div>
-                    <StyledText mb="1rem">
-                      <GiBubblingBowl
-                        size="1.25rem"
-                        color={theme.colors.text}
-                      />
-                      Modifications:
-                    </StyledText>
-                    {recipe.modifications.map((recipe) => {
-                      return (
-                        <div key={recipe.title}>
-                          <span className="serves">{recipe.title}</span>
-                          <ul>
-                            {recipe.list.map((m) => (
-                              <li key={m}>{m}</li>
-                            ))}
-                          </ul>
-                        </div>
-                      );
-                    })}
-                  </div>
-                )}
-              </div>
-            </div>
-            <div className="instructions">
-              <StyledText>Instructions:</StyledText>
+            <div>
+              <StyledText>
+                <GiFruitBowl size="1.25rem" color={theme.colors.text} />
+                Ingridients:
+              </StyledText>
+              {recipe.servings && (
+                <span className="serves">
+                  (for {recipe.servings} servings{' '}
+                  <GiKnifeFork size=".75rem" color={theme.colors.mutedText} />)
+                </span>
+              )}
               <ul>
-                {Object.values(recipe.instructions).map((value, index) => {
+                {Object.values(recipe.ingridients).map((value, index) => {
                   return <li key={index}>{value}</li>;
                 })}
               </ul>
+
+              {recipe.modifications && (
+                <div>
+                  <StyledText mb="1rem">
+                    <GiBubblingBowl size="1.25rem" color={theme.colors.text} />
+                    Modifications:
+                  </StyledText>
+                  {recipe.modifications.map((recipe) => {
+                    return (
+                      <div key={recipe.title}>
+                        <span className="serves">{recipe.title}</span>
+                        <ul>
+                          {recipe.list.map((m) => (
+                            <li key={m}>{m}</li>
+                          ))}
+                        </ul>
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
             </div>
-            {!!recipe.video && (
-              <>
-                <h4>Watch the Video Below</h4>
-                <StyledVideoWrapper>
-                  <ReactPlayer url={recipe.video} controls />
-                </StyledVideoWrapper>
-              </>
-            )}
-            {navigator.share && (
-              <StyledFlex>
-                <StyledButton linear onClick={shareAPI} mb="1rem">
-                  share recipe <FaShareAlt />
-                </StyledButton>
-              </StyledFlex>
-            )}
-          </StyledRecipeDetail>
-        </StyledContainer>
-      </BaseLayout>
-    </>
+          </div>
+          <div className="instructions">
+            <StyledText>Instructions:</StyledText>
+            <ul>
+              {Object.values(recipe.instructions).map((value, index) => {
+                return <li key={index}>{value}</li>;
+              })}
+            </ul>
+          </div>
+          {!!recipe.video && (
+            <>
+              <h4>Watch the Video Below</h4>
+              <StyledVideoWrapper>
+                <ReactPlayer url={recipe.video} controls />
+              </StyledVideoWrapper>
+            </>
+          )}
+          {navigator.share && (
+            <StyledFlex>
+              <StyledButton linear onClick={shareAPI} mb="1rem">
+                share recipe <FaShareAlt />
+              </StyledButton>
+            </StyledFlex>
+          )}
+        </StyledRecipeDetail>
+      </StyledContainer>
+    </BaseLayout>
   );
 };
 
 export default Recipe;
 
-export const getStaticProps = async () => {
+export async function getStaticProps({ params }) {
+  const currentRecipe = RECIPES_LIST[params.slug - 1];
   return {
-    props: {},
+    props: { currentRecipe },
   };
-};
-const ten = { params: { slug: '10' } };
-export const getStaticPaths = async (ctx) => {
+}
+export async function getStaticPaths() {
+  const paths = Object.values(RECIPES_LIST).map((value) => {
+    return { params: { slug: value.id.toString() } };
+  });
   return {
-    paths: [
-      { params: { slug: '1' } },
-      { params: { slug: '2' } },
-      { params: { slug: '3' } },
-      { params: { slug: '4' } },
-      { params: { slug: '5' } },
-      { params: { slug: '6' } },
-      { params: { slug: '7' } },
-      { params: { slug: '8' } },
-      { params: { slug: '9' } },
-      ten,
-    ],
+    paths,
     fallback: true,
   };
-};
+}
